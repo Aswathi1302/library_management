742234a37e28e27a9987fa5c3b057c05bf949b5f
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-02 12:02:13 +0530

    first commit

---
+++ book_details.py
@@ -0,0 +1,22 @@
+while True:
+    print("select an option from the menu")
+    print("1. add book:-")
+    print("2.view book:-")
+    print("3.search book:-")
+    print("4.upadte book:-")
+    print("5. delete book:-")
+    print("6.exit")
+
+    choice=int(input("enter your choice:-"))
+    if(choice==1):
+        print("add book:")
+    elif(choice==2):
+        print("view book")
+    elif(choice==3):
+        print("search a book")  
+    elif(choice==4):
+        print("update book")
+    elif(choice==5):
+        print("delete employe")  
+    elif(choice==6):
+        break      
